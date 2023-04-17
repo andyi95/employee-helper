4da442be9761a1385f2ba96cdf78352bdae2f252
Andrey Chernikov <linuxoid.andrew@gmail.com>
2023-04-17 07:01:11 +0300

    Initial frontend

---
+++ frontend/vue.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    chainWebpack: config => {
+        config.module
+            .rule('vue')
+            .use('vue-loader')
+            // .loader('vue-loader')
+            .tap(options => {
+                options.complierOptions = {whitespace: 'preserve'};
+                // options.prettify = false;
+                return options;
+            });
+    },
+    devServer: {
+        host: 'localhost',
+        port: 4000,
+        allowedHosts: [
+            'frontend',
+            'localhost',
+            'lk-employee.dev.andyi95.com'
+        ],
+        client: {
+            webSocketURL: 'auto://0.0.0.0:0/ws'
+        }
+    }
+}
